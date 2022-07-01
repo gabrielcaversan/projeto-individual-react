8cf273759dbf2a900460a916538599a8c505e2eb
gabrielcaversan <gabriel.felipe.caversan@gmail.com>
2022-07-01 11:58:20 -0300

    Ajustes no Modal e comentários para estudo

--- src/components/ListaUsuarios/ListaUsuarios.jsx
+++ src/components/ListaUsuarios/ListaUsuarios.jsx
@@ -7,12 +7,11 @@ export default function ListaUsuarios() {
 
     //useEffect é chamado apenas após a renderização da tela.
     //Primeiro indica onde ele irá buscar (no caso a URL). Depois (then) transforma o retorno em um Json. Depois (outro then) pego esse retorno json e envio para o state usuarios(setUsuarios), ou seja, disponibilizo os dados da URL na variavel usuarios.
-    useEffect(
-        () => {
+    useEffect(() => {
         fetch('https://www.mocky.io/v2/5d531c4f2e0000620081ddce')
             .then( (resp) => resp.json())
             .then((json) => {setUsuarios(json)})
-    });
+    }, []);
 
     //Utilizo o map para transformar os dados (lista usuarios), coloco um argumento (itensUsuario) e puxo eles pelas propriedades do objeto da URL (id, name, img e username). A key é um identificador em elementos para controlar o estado da lista, precisa ser um número que não se altera, por isso utilizei o mesmo valor do id.
     return (
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,43 +1,36 @@
-import React, { useState } from "react";
+import React from "react";
 import './modal.css';
 import { cartoes } from '../Cartoes/Cartoes';
 import { Mascara } from "../Mascara/Mascara";
 
 const Modal = (props) => {
-
-    const [pagamento, setPagamento] = useState(false);
-
     const dadosCartao = async (evento) => {
         evento.preventDefault();
 
-        const formData = new FormData(evento.target);
-        const valor = formData.get("inputPagamento")
-        const cartao = formData.get("cartaoEscolhido");
-        const selecaoCartao = cartoes.find((cartaoPropriedades) => cartaoPropriedades.numero_cartao === cartao);
+        //Utilizando o .find(), eu faço com que cada um dos objetos da array fiquem dentro de (itemCartao), dessa forma eu coloco uma condição para que se o valor da propriedade "numero_cartao" for igual ao valor do select, ele me retorna aquele objeto. Dessa forma consigo depois coletar informações dentro do objeto usando selecionaCartao.nomeDaPropriedade.
+        const selecionaCartao = cartoes.find((itemCartao) => {return itemCartao.numero_cartao === evento.target.elements["cartaoEscolhido"].value});
 
-        await (
-            await fetch("https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989",
-                {
-                    method: "POST",
-                    body: {
-                        numero_cartao: cartao,
-                        cvv: selecaoCartao.cvv,
-                        data_expiracao: selecaoCartao.data_expiracao,
+        //Agora eu faço uma requisição HTTP e pego os dados da URL utilizando fetch (ou seja, obtenho dados sem que a pagina precise ser carregada). Por estar usando uma função assincrona, posso armazenar o resultado do fetch em uma const response. Porém, antes da invocação do fetch, utilizo a AWAIT. O que significa que enquanto a resposta da requisição não chegar, nenhum código abaixo de AWAIT será executado. E quando a resposta da invocação for obtida, será resultado em uma PROMISE e o AWAIT vai atribuir para a const response só o valor resolvido dessa PROMISE. E para obter de fato o corpo da resposta, precisamos formatar para JSON. Por isso abaixo insiro a const data, que recebe AWAIT response.json. Esse AWAIT é necessário pois o response.json irá resultar em uma PROMISE, ou seja, o AWAIT espera a PROMISE ser resolvida e atribui apenas o valor resolvido para a const data.
+        const response = await fetch("https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989", {
+            method: "POST",
+            body:{
+                numero_cartao: selecionaCartao.numero_cartao,
+                cvv: selecionaCartao.cvv,
+                data_expiracao: selecionaCartao.data_expiracao,
 
-                        value: valor,
-                    },
-                })
-        ).json();
+                value: evento.target.elements["inputPagamento"],
+                },
+        })
+        const data = await response.json()
+        console.log(data)
 
-
-        //Condição que verifica se o numero do cartão é igual ao cartão válido na API. Se for true, será exibido a mensagem de aprovado, se for false, será exibido a mensagem de recusado.
+        //Condição que verifica se o valor do select é igual ao cartão válido na API. Se for true, será exibido a mensagem de aprovado, se for false, será exibido a mensagem de recusado.
+        const cartao = evento.target.elements["cartaoEscolhido"].value
         if (cartao === "1111111111111111") {
-            setPagamento(true);
             document.querySelector('.pagamento-cliente').innerHTML = 'Recibo de pagamento'
             document.querySelector('.formulario-pagamento').style.display = 'none';
             document.querySelector('.aprovado').style.display = 'block';
         } else {
-            setPagamento(true);
             document.querySelector('.pagamento-cliente').innerHTML = 'Recibo de pagamento'
             document.querySelector('.formulario-pagamento').style.display = 'none';
             document.querySelector('.recusado').style.display = 'block';
